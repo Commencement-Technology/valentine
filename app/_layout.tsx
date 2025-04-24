f1c3de9a8e9e34523bbd6e6a8b1ea88ffae0128e
Aakash-Srinivasan <aakash4367595@gmail.com>
2025-04-24 18:35:09 +0530

    final dev commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="Favourites" />
         <Stack.Screen name="LoginScreen" />
-        <Stack.Screen name="FeedbackScreen" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
